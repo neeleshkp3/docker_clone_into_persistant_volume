93eb1245b673b9ba9475229d10df978f2255c5ad
neeleshkp3 <neelesh.k@sarv.com>
2019-12-13 14:17:42 +0530

    docker_clone_volume.sh

---
+++ README.md
@@ -0,0 +1,24 @@
+# docker-convenience-scripts
+
+This repository will contain different convenience scripts for docker I have gathered over time.
+
+## docker_clone_volume.sh
+
+The purpose for this script is that I can easily create a clone of an existing docker data with
+a new name. This will allow me to create a duplicate of an existing data volume I use in the
+production environment of my blog for example and take that duplicate to my development version 
+to ensure I have the latest production data also at development.
+
+You can find more details in my blog post [Cloning Docker Data Volumes](https://www.guidodiepen.nl/2016/05/cloning-docker-data-volumes/)
+
+## docker_get_data_volume_info.sh
+
+The purpose for this script is that I can easily get a list of details for all data volumes 
+that are currently present. The information that is provided to the user as output is per 
+data volume the current size of the data volume and a list of stopped or running containers 
+that have a link to this data volume, including the image corresponding to the container.
+The script allows me to easily see if there are some data volumes on my disk that are taking
+up a lot of space and are not needed anymore.
+
+You can find more details in my blog post [Listing information for all your named/unnamed data volumes](https://www.guidodiepen.nl/2017/04/listing-information-for-all-your-named-unnamed-data-volumes/)
+# docker_clone_into_persistant_volume By Neelesh Kant Pandey
---
+++ docker_clone_volume.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#Author: Guido Diepen
+
+#Convenience script that can help me to easily create a clone of a given
+#data volume. The script is mainly useful if you are using named volumes
+
+
+#First check if the user provided all needed arguments
+if [ "$1" = "" ]
+then
+        echo "Please provide a source volume name"
+        exit
+fi
+
+if [ "$2" = "" ] 
+then
+        echo "Please provide a destination volume name"
+        exit
+fi
+
+if [ "$3" = "" ]
+then
+        echo "Please provide a destination mount point directory in which volume will be created"
+        exit
+fi
+
+DIR=$3
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "Creating Persistant Volume named $2 in $3/"
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Error: ${DIR} not found. Can not continue."
+  exit 1
+fi
+
+#Check if the source volume name does exist
+docker volume inspect $1 > /dev/null 2>&1
+if [ "$?" != "0" ]
+then
+        echo "The source volume \"$1\" does not exist"
+        exit
+fi
+
+#Now check if the destinatin volume name does not yet exist
+docker volume inspect $2 > /dev/null 2>&1
+
+if [ "$?" = "0" ]
+then
+        echo "The destination volume \"$2\" already exists"
+        exit
+fi
+
+
+
+echo "Creating destination volume \"$2\"..."
+#docker volume create --name $2  
+docker volume create -d local-persist -o mountpoint=$3/$2 --name $2
+echo "Copying data from source volume \"$1\" to destination volume \"$2\"..."
+docker run --rm \
+           -i \
+           -t \
+           -v $1:/from \
+           -v $2:/to \
+           alpine ash -c "cd /from ; cp -av . /to"
